e2b32c5882ceb36f18b3e9ad010ae60bc9d50c96
Jim Gilbert <jgilbert@akamai.com>
2020-08-06 12:37:36 -0400

    add /helloworld

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 app.use(express.static('public'))
 app.get('/about', function (req, res) {
-    res.send('about')
+    res.send('Edge DNS Rocks!')
+})
+app.get('/helloworld', function (req, res) {
+    res.send('Edge DNS says hello!')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
