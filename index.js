1ec68cf6069db204baf988af6abed8e7939c8da2
Jim Gilbert <jgilbert@akamai.com>
2020-08-06 12:19:51 -0400

    add a /about path

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ require('newrelic')
 const express = require('express')
 const app = express()
 app.use(express.static('public'))
+app.get('/about', function (req, res) {
+    res.send('about')
+})
 app.listen(3000, () => console.log('Server running on port 3000'))
